0f24ef0713610cca76dce25d6717791844ff75c3
BenardBett <kiplangatbett5@gmail.com>
2020-12-06 16:52:49 +0300

    created a class for pitches

--- manage.py
+++ manage.py
@@ -1,6 +1,6 @@
 from app import create_app,db
 from flask_script import Manager,Server
-from app.models import PitchCategory
+from app.models import  User,PitchCategory,Pitch
 
 # Creating app instance
 app = create_app('development')
@@ -17,6 +17,6 @@ def test():
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app,db = db,PitchCategory=PitchCategory)
+    return dict(app = app,db = db,User=User, PitchCategory=PitchCategory)
 if __name__ == '__main__':
     manager.run()
