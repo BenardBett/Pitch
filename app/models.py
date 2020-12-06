4c6a5d6cdc37abf3a1202977c4215745018243ff
BenardBett <kiplangatbett5@gmail.com>
2020-12-06 16:32:52 +0300

    created a function that saves category

--- app/models.py
+++ app/models.py
@@ -6,6 +6,18 @@ class PitchCategory(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
+    
+    
+    def save_category(self):
+        db.session.add(self)
+        db.session.commit()
+
+
+    @classmethod
+    def get_categories(cls):
+        categories = PitchCategory.query.all()
+        return categories
+
 
     def __repr__(self):
         return f'User {self.username}'
--- config.py
+++ config.py
@@ -5,16 +5,20 @@ class Config:
     MOVIE_API_BASE_URL ='https://api.themoviedb.org/3/movie/{}?api_key={}'
     MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/watchlist'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/pitch'
+
 
 
 
 
 class ProdConfig(Config):
-    pass
+    DEBUG = False
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
 
 class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI="postgres://moringa:Access@localhost/pitch"
+    DEVELOPMENT = True
     DEBUG = True
 
 config_options = {
