44e4ac0920e58146417d7eb9f8870866e8789456
BenardBett <kiplangatbett5@gmail.com>
2020-12-06 22:39:25 +0300

    added a new column for hashing

--- app/models.py
+++ app/models.py
@@ -9,7 +9,7 @@ class User(db.Model):
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
     email = db.Column(db.String(255),unique = True, index =True)
-   
+    pass_secure = db.Column(db.String(255))
 
     
 
