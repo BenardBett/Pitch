71df90ae9fbe75d876e80578efcd748cacad5de4
BenardBett <kiplangatbett5@gmail.com>
2020-12-06 17:14:12 +0300

    import migrate and migrate command class as well as initialized it

--- manage.py
+++ manage.py
@@ -1,12 +1,16 @@
 from app import create_app,db
 from flask_script import Manager,Server
 from app.models import  User,PitchCategory,Pitch
-
+from  flask_migrate import Migrate, MigrateCommand
 # Creating app instance
 app = create_app('development')
 
 manager = Manager(app)
 manager.add_command('server',Server)
+
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
+
 
 @manager.command
 def test():
@@ -16,7 +20,8 @@ def test():
     unittest.TextTestRunner(verbosity=2).run(tests)
 
 @manager.shell
-def make_shell_context():
+def make_shell_context():igrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
     return dict(app = app,db = db,User=User, PitchCategory=PitchCategory)
 if __name__ == '__main__':
     manager.run()
