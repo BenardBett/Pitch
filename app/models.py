5f4ca2b30e2e3a3a46f0cc3d88d388111dd31a30
BenardBett <kiplangatbett5@gmail.com>
2020-12-07 06:21:09 +0300

    created a new file for registering new users in auth package

--- app/models.py
+++ app/models.py
@@ -1,7 +1,12 @@
 from . import db
 from werkzeug.security import generate_password_hash,check_password_hash
-   
-class User(db.Model):
+from . import login_manager
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class User(UserMixin,db.Model):
     """ class modelling the users """
 
     __tablename__='users'
@@ -9,7 +14,7 @@ class User(db.Model):
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
     email = db.Column(db.String(255),unique = True, index =True)
-    pass_secure = db.Column(db.String(255))
+    pass_hash = db.Column(db.String(255))
 
    pass_secure  = db.Column(db.String(255))
 
